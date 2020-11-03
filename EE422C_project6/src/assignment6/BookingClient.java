c698850d4a78d928055ad09843012c8cca6a0cb2
mina abbassian <mina.abbassian@gmail.com>
2020-11-03 12:59:22 -0600

    added starter code

---
+++ EE422C_project6/src/assignment6/BookingClient.java
@@ -0,0 +1,41 @@
+/* MULTITHREADING <MyClass.java>
+ * EE422C Project 6 submission by
+ * Replace <...> with your actual data.
+ * <Student Name>
+ * <Student EID>
+ * <Student 5-digit Unique No.>
+ * Slip days used: <0>
+ * Fall 2020
+ */
+package assignment6;
+
+import java.util.Map;
+import java.util.List;
+import java.lang.Thread;
+
+public class BookingClient {
+
+    /**
+     * @param office  maps box office id to number of customers in line
+     * @param theater the theater where the show is playing
+     */
+    public BookingClient(Map<String, Integer> office, Theater theater) {
+        // TODO: Implement this constructor
+    }
+
+    /**
+     * Starts the box office simulation by creating (and starting) threads
+     * for each box office to sell tickets for the given theater
+     *
+     * @return list of threads used in the simulation,
+     * should have as many threads as there are box offices
+     */
+    public List<Thread> simulate() {
+        // TODO: Implement this method
+        return null;
+    }
+
+    public static void main(String[] args) {
+        // TODO: Initialize test data to description
+    }
+}
